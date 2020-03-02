08ec603058f5f2e001f9fa00f561f3a75686294b
Keelah <foxlider.atom@gmail.com>
2020-03-02 16:55:59 +0100

    Add project files.

---
+++ HeartMonitorWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using LiveCharts.Geared;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HeartMonitorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
